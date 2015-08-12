a9f47d60f939e082ffa0e512711e14af726da9f0
Ashik Ahmed <ashik.992@gmail.com>
2015-08-12 22:56:48 +0600

    Pretty print

--- termLyrics/termLyrics.py
+++ termLyrics/termLyrics.py
@@ -1,15 +1,33 @@
 import urllib
 import re
+class color:
+  PURPLE = '\033[95m'
+  CYAN = '\033[96m'
+  DARKCYAN = '\033[36m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  END = '\033[0m'
+
 
 def extractLyrics(pageTxt):
+  lyric = []
+  songTitle = re.search(r'<h1 .*>(.*?)</h1>', pageTxt, re.DOTALL)
+  songTitle = songTitle.group(1).replace('Lyrics', '')
+  lyric.append(songTitle)
+
   lTxt = re.search(r'<div id="lyrics-body-text">(.*?)</div>', pageTxt, re.DOTALL)
   lTxt = lTxt.group(1)
   lP = re.sub(r'</?p.*?>', '\n', lTxt)
-  lyric = re.sub(r'<.*?>', '', lP)
+  lyric.append(re.sub(r'<.*?>', '', lP))
   return lyric
 
 
 
-urlFile = urllib.urlopen('http://www.metrolyrics.com/bad-blood-lyrics-taylor-swift.html')
+urlFile = urllib.urlopen('http://www.metrolyrics.com/highway-to-hell-lyrics-acdc.html')
 lyrics = extractLyrics(urlFile.read())
-print lyrics
+print color.RED+color.UNDERLINE+lyrics[0].strip()+color.END+color.END
+print color.GREEN+lyrics[1]+color.END
